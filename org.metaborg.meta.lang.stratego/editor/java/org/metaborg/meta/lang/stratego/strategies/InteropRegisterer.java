c5de998e2133bc03a74800d32e4f8481d1c4c8c5
Gabriel Konat <gabrielkonat@gmail.com>
2015-07-08 17:46:30 -0700

    Use Spoofax core source/includes to find other files and imports.

--- org.metaborg.meta.lang.stratego/editor/java/org/metaborg/meta/lang/stratego/strategies/InteropRegisterer.java
+++ org.metaborg.meta.lang.stratego/editor/java/org/metaborg/meta/lang/stratego/strategies/InteropRegisterer.java
@@ -4,7 +4,7 @@ import org.strategoxt.lang.JavaInteropRegisterer;
 import org.strategoxt.lang.Strategy;
 
 public class InteropRegisterer extends JavaInteropRegisterer {
-    public InteropRegisterer() {
-        super(new Strategy[] { java_load_properties_0_0.instance, java_interpolate_environment_variables_0_0.instance });
-    }
+	public InteropRegisterer() {
+		super(new Strategy[] {});
+	}
 }
