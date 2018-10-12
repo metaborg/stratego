358947187f98dfd4b3de7a5d7cbcc34dea6b8a4d
Jeff Smits <jeff.smits@gmail.com>
2018-10-12 11:14:17 +0200

    Output one or more SDefTs without cifying the names and combine those with the pack tool

--- stratego.compiler.pack/src/main/java/mb/stratego/compiler/pack/Packer.java
+++ stratego.compiler.pack/src/main/java/mb/stratego/compiler/pack/Packer.java
@@ -7,7 +7,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
-import java.util.Arrays;
+//import java.util.Arrays;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
 
@@ -32,43 +32,11 @@ public class Packer {
      *             When there is a file system problem
      */
     public static void packStrategy(Path inputDir, @Nullable Path outputFile, String strategyName) throws IOException {
-        // find the latest underscore and assume the term variable count is between that
-        // and the end of the string
-        int uLast = strategyName.lastIndexOf('_');
-        int tvars = Integer.parseInt(strategyName.substring(uLast + 1, strategyName.length()));
-        // find the underscore before that and assume the strategy variable count is
-        // between that and the latest underscore
-        int uSecondLast = strategyName.lastIndexOf('_', uLast - 1);
-        int svars = Integer.parseInt(strategyName.substring(uSecondLast + 1, uLast));
-
         if(outputFile == null) {
             outputFile = Paths.get(inputDir.getParent().getParent().toString(), "stratego.compiler.pack", strategyName + ".ctree");
         }
         Files.createDirectories(outputFile.getParent());
 
-        packStrategy(inputDir, outputFile, strategyName, svars, tvars);
-    }
-
-    /**
-     * Packs StrategoCore strategy bodies in a directory into a single strategy
-     * definition in a file
-     * 
-     * @param inputDir
-     *            Directory that only includes .aterm files with the strategy bodies
-     * @param outputFile
-     *            The file to output to. This file will be truncated if one already exists.
-     * @param strategyName
-     *            The strategy name to use for the strategy definition, expected to
-     *            end with _a_b where a and b are the number of strategy and term
-     *            variables resp.
-     * @param svars
-     *            the a in the above string
-     * @param tvars
-     *            the b in the above string
-     * @throws IOException
-     *             When there is a file system problem
-     */
-    public static void packStrategy(Path inputDir, Path outputFile, String strategyName, int svars, int tvars) throws IOException {
         // We use the nio.Channel API here because it is supposed to be the fastest way
         // to append one file to another
         // This may need to be changed when using this tool inside Spoofax, dealing with
@@ -77,32 +45,25 @@ public class Packer {
         try (final FileChannel outChannel = FileChannel.open(outputFile, StandardOpenOption.CREATE,
                 StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.WRITE)) {
             write(outChannel, SPEC_START);
-            write(outChannel, sdeftStart(strategyName, svars, tvars));
 
             // We need to be careful not to include the outputFile among the input files in
             // the directory
             try (Stream<Path> inputFiles = Files.list(inputDir).filter(relevantFiles(outputFile))) {
-                int num_defs = 0;
                 // Iterable instead of Stream.forEach, because of exceptions and counting
                 // num_defs
                 Iterable<Path> iterable = () -> inputFiles.iterator();
-                String next_line = "GuardedLChoice(\n";
+                String next_line = "";
                 for (Path inputFile : iterable) {
-                    num_defs++;
                     write(outChannel, next_line);
                     try (final FileChannel inChannel = FileChannel.open(inputFile, StandardOpenOption.READ)) {
                         transferContents(inChannel, outChannel);
                     }
-                    next_line = "\n, Id(), GuardedLChoice(\n";
+                    next_line = "\n,";
                 }
-                if (num_defs == 0) {
+                if (next_line == "") {
                     // When there are no relevant files, close the file
                     break openFile;
                 }
-                write(outChannel, "\n, Id(), Id())");
-                byte[] closing = new byte[num_defs];
-                Arrays.fill(closing, (byte) ')');
-                write(outChannel, closing);
             }
             write(outChannel, SPEC_END);
             return; // success
@@ -185,57 +146,4 @@ public class Packer {
             transfered += channel.write(buffer);
         }
     }
-
-    /**
-     * @param strategyName
-     * @param svars
-     *            amount of strategy variables
-     * @param tvars
-     *            amount of term variables
-     * @return the start of the CTree for a strategy definition with the given name
-     *         and argument amounts
-     */
-    private static String sdeftStart(String strategyName, int svars, int tvars) {
-        final StringBuilder out = new StringBuilder();
-
-        out.append("SDefT(\"");
-        out.append(strategyName);
-        out.append("\", [");
-        String next_line = "";
-        for (int i = 0; i < svars; i++) {
-            out.append(next_line);
-            out.append(svarArg(i));
-            next_line = ", ";
-        }
-        out.append("], [");
-        next_line = "";
-        for (int i = 0; i < tvars; i++) {
-            out.append(next_line);
-            out.append(tvarArg(i));
-            next_line = ", ";
-        }
-        out.append("], \n");
-        return out.toString();
-    }
-
-    /**
-     * @param i
-     *            index of the term variable
-     * @return CTree for term variable argument of a strategy definition, with
-     *         generic type
-     */
-    private static String tvarArg(int i) {
-        return "VarDec(\"tvar_" + i + "\",ConstType(Sort(\"ATerm\",[])))";
-    }
-
-    /**
-     * @param i
-     *            index of the strategy variable
-     * @return CTree for strategy variable argument of a strategy definition, with
-     *         generic type
-     */
-    private static String svarArg(int i) {
-        return "VarDec(\"svar_" + i + "\",FunType([ConstType(Sort(\"ATerm\",[]))],ConstType(Sort(\"ATerm\",[]))))";
-    }
-
 }
