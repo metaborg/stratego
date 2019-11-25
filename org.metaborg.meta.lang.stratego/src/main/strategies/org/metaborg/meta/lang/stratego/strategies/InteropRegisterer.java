9874840894dbb62a5f4d46c5e573bf6446106b89
Jeff Smits <mail@jeffsmits.net>
2019-11-25 17:02:28 +0100

    Remove external that used to be used in the stratego incremental compiler frontend

--- org.metaborg.meta.lang.stratego/src/main/strategies/org/metaborg/meta/lang/stratego/strategies/InteropRegisterer.java
+++ org.metaborg.meta.lang.stratego/src/main/strategies/org/metaborg/meta/lang/stratego/strategies/InteropRegisterer.java
@@ -5,6 +5,6 @@ import org.strategoxt.lang.Strategy;
 
 public class InteropRegisterer extends JavaInteropRegisterer {
     public InteropRegisterer() {
-        super(new Strategy[] { nano_time_real_0_0.instance });
+        super(new Strategy[] { });
     }
 }
