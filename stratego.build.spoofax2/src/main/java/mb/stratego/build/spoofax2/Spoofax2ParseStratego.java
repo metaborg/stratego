0da32a201060ce93e86119a183b3011f4669ee41
Gabriel Konat <gabrielkonat@gmail.com>
2020-07-13 16:18:52 +0200

    Fix Stratego parsing not working.

--- stratego.build.spoofax2/src/main/java/mb/stratego/build/spoofax2/Spoofax2ParseStratego.java
+++ stratego.build.spoofax2/src/main/java/mb/stratego/build/spoofax2/Spoofax2ParseStratego.java
@@ -1,17 +1,19 @@
 package mb.stratego.build.spoofax2;
 
-import mb.pie.api.ExecException;
-import mb.stratego.build.strincr.ParseStratego;
-import mb.stratego.build.termvisitors.DisambiguateAsAnno;
-import mb.stratego.build.util.StrIncrContext;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+
+import javax.annotation.Nullable;
+import javax.inject.Inject;
+
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.vfs2.FileObject;
 import org.metaborg.core.config.JSGLRVersion;
+import org.metaborg.core.language.ILanguage;
 import org.metaborg.core.language.ILanguageIdentifierService;
 import org.metaborg.core.language.ILanguageImpl;
 import org.metaborg.core.language.ILanguageService;
 import org.metaborg.core.language.IdentifiedResource;
-import org.metaborg.core.language.LanguageIdentifier;
 import org.metaborg.core.resource.IResourceService;
 import org.metaborg.spoofax.core.SpoofaxConstants;
 import org.metaborg.spoofax.core.syntax.ISpoofaxSyntaxService;
@@ -24,10 +26,10 @@ import org.spoofax.interpreter.terms.ITermFactory;
 import org.spoofax.terms.io.binary.TermReader;
 import org.spoofax.terms.util.TermUtils;
 
-import javax.annotation.Nullable;
-import javax.inject.Inject;
-import java.io.InputStream;
-import java.nio.charset.Charset;
+import mb.pie.api.ExecException;
+import mb.stratego.build.strincr.ParseStratego;
+import mb.stratego.build.termvisitors.DisambiguateAsAnno;
+import mb.stratego.build.util.StrIncrContext;
 
 public class Spoofax2ParseStratego implements ParseStratego {
     private final IResourceService resourceService;
@@ -38,15 +40,10 @@ public class Spoofax2ParseStratego implements ParseStratego {
     private final ISpoofaxSyntaxService syntaxService;
     private final StrIncrContext strContext;
 
-    @Inject public Spoofax2ParseStratego(
-        IResourceService resourceService,
-        ILanguageIdentifierService languageIdentifierService,
-        ILanguageService languageService,
-        ITermFactory termFactory,
-        ISpoofaxUnitService unitService,
-        ISpoofaxSyntaxService syntaxService,
-        StrIncrContext strContext
-    ) {
+    @Inject public Spoofax2ParseStratego(IResourceService resourceService,
+        ILanguageIdentifierService languageIdentifierService, ILanguageService languageService,
+        ITermFactory termFactory, ISpoofaxUnitService unitService, ISpoofaxSyntaxService syntaxService,
+        StrIncrContext strContext) {
         this.resourceService = resourceService;
         this.languageIdentifierService = languageIdentifierService;
         this.languageService = languageService;
@@ -56,8 +53,8 @@ public class Spoofax2ParseStratego implements ParseStratego {
         this.strContext = strContext;
     }
 
-    @Override
-    public IStrategoTerm parse(InputStream inputStream, Charset charset, @Nullable String path) throws Exception {
+    @Override public IStrategoTerm parse(InputStream inputStream, Charset charset, @Nullable String path)
+        throws Exception {
         final @Nullable FileObject inputFile;
         if(path != null) {
             inputFile = resourceService.resolve(path);
@@ -65,31 +62,29 @@ public class Spoofax2ParseStratego implements ParseStratego {
             inputFile = null;
         }
 
-        final LanguageIdentifier strategoLanguageId = LanguageIdentifier.parse(SpoofaxConstants.LANG_STRATEGO_ID);
-        final @Nullable ILanguageImpl strategoLang;
-        final @Nullable ILanguageImpl strategoDialect;
+        @Nullable ILanguageImpl strategoLangImpl = null;
+        @Nullable ILanguageImpl strategoDialect = null;
         if(inputFile != null) {
             final @Nullable IdentifiedResource identified = languageIdentifierService.identifyToResource(inputFile);
             if(identified != null) {
-                strategoLang = identified.language;
+                strategoLangImpl = identified.language;
                 strategoDialect = identified.dialect;
-            } else {
-                strategoLang = languageService.getImpl(strategoLanguageId);
-                strategoDialect = null;
             }
-        } else {
-            strategoLang = languageService.getImpl(strategoLanguageId);
-            strategoDialect = null;
         }
-
-        if(strategoLang == null) {
+        if(strategoLangImpl == null) {
+            final @Nullable ILanguage strategoLang = languageService.getLanguage(SpoofaxConstants.LANG_STRATEGO_NAME);
+            if(strategoLang != null) {
+                strategoLangImpl = strategoLang.activeImpl();
+            }
+        }
+        if(strategoLangImpl == null) {
             throw new ExecException("Cannot find/load Stratego language. Please add a source dependency "
                 + "'org.metaborg:org.metaborg.meta.lang.stratego:${metaborgVersion}' in your metaborg.yaml file. ");
         }
 
         @Nullable IStrategoTerm ast;
         final String text = IOUtils.toString(inputStream, charset);
-        final ISpoofaxInputUnit inputUnit = unitService.inputUnit(inputFile, text, strategoLang, strategoDialect);
+        final ISpoofaxInputUnit inputUnit = unitService.inputUnit(inputFile, text, strategoLangImpl, strategoDialect);
         final ISpoofaxParseUnit parseResult = syntaxService.parse(inputUnit, JSGLRVersion.v2);
         ast = parseResult.ast();
         if(!parseResult.success() || ast == null) {
@@ -102,12 +97,10 @@ public class Spoofax2ParseStratego implements ParseStratego {
         return ast;
     }
 
-    @Override
-    public IStrategoTerm parseRtree(InputStream inputStream) throws Exception {
+    @Override public IStrategoTerm parseRtree(InputStream inputStream) throws Exception {
         final IStrategoTerm ast = new TermReader(termFactory).parseFromStream(inputStream);
-        if(!(TermUtils.isAppl(ast) && ((IStrategoAppl)ast).getName().equals("Module")
-            && ast.getSubtermCount() == 2)) {
-            if(!(TermUtils.isAppl(ast) && ((IStrategoAppl)ast).getName().equals("Specification")
+        if(!(TermUtils.isAppl(ast) && ((IStrategoAppl) ast).getName().equals("Module") && ast.getSubtermCount() == 2)) {
+            if(!(TermUtils.isAppl(ast) && ((IStrategoAppl) ast).getName().equals("Specification")
                 && ast.getSubtermCount() == 1)) {
                 throw new ExecException("Did not find Module/2 in RTree file. Found: \n" + ast.toString(2));
             } else {
