be0933678bc8c9ae1df83c3acc527f050426665f
Toine Hartman <tjbhartman@gmail.com>
2021-07-01 16:11:53 +0200

    Add data analysis

--- stratego.build.spoofax2.benchmark/src/main/java/benchmark/stratego2/compilation/Bubblesort.java
+++ stratego.build.spoofax2.benchmark/src/main/java/benchmark/stratego2/compilation/Bubblesort.java
@@ -6,7 +6,7 @@ import org.openjdk.jmh.annotations.Param;
 
 public class Bubblesort extends CompilationBenchmark implements BubblesortProblem {
 
-    @Param({"10", "20", "50", "100", "200", "300", "500", "720", "1000"})
+    @Param({"10", "20", "50", "100", "200"/*, "300", "500", "720", "1000"*/})
     int problemSize;
 
 }
