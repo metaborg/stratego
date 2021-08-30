f65c6d0d6830a5267b5288ea0906b861b23d3fb9
Toine Hartman <tjbhartman@gmail.com>
2021-08-30 15:14:20 +0200

    Update benchmark (results)

--- stratego.build.spoofax2.benchmark/src/main/java/benchmark/stratego2/execution/Hanoi.java
+++ stratego.build.spoofax2.benchmark/src/main/java/benchmark/stratego2/execution/Hanoi.java
@@ -3,7 +3,11 @@ package benchmark.stratego2.execution;
 import benchmark.stratego2.problem.HanoiProblem;
 import benchmark.stratego2.template.benchmark.ExecutionBenchmark;
 import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Timeout;
 
+import java.util.concurrent.TimeUnit;
+
+@Timeout(time = 30, timeUnit = TimeUnit.MINUTES)
 public class Hanoi extends ExecutionBenchmark implements HanoiProblem {
 
     @Param({"4", "8", "12", "16", "20"})
