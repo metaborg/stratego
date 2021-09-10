6cffb25d768e44c328adb5b43908ccbe5a83496a
Toine Hartman <tjbhartman@gmail.com>
2021-09-10 13:02:59 +0200

    Add Benchexpr problem instances

--- stratego.build.spoofax2.benchmark/src/main/java/benchmark/stratego2/execution/Benchexpr.java
+++ stratego.build.spoofax2.benchmark/src/main/java/benchmark/stratego2/execution/Benchexpr.java
@@ -6,7 +6,7 @@ import org.openjdk.jmh.annotations.Param;
 
 public class Benchexpr extends ExecutionBenchmark implements BenchexprProblem {
 
-    @Param({"10", "15", "20", "22"})
+    @Param({"10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "22"})
     int problemSize;
 
 }
