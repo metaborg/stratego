699311fdc765f7e176fe10d73ca543f6875eb8e1
Toine Hartman <tjbhartman@gmail.com>
2021-09-15 13:12:00 +0200

    Adapt to PIE API changes

--- stratego.build.spoofax2.benchmark/src/main/java/api/Compiler.java
+++ stratego.build.spoofax2.benchmark/src/main/java/api/Compiler.java
@@ -161,7 +161,7 @@ public class Compiler {
         final PieBuilder pieBuilder = new PieBuilderImpl();
         pieBuilder.withLoggerFactory(StreamLoggerFactory.stdErrVeryVerbose());
         pieBuilder.withStoreFactory(
-                (serde, resourceService, loggerFactory) -> new SerializingStore<>(serde,
+                (serde, resourceService, loggerFactory) -> new SerializingStore<>(serde, loggerFactory,
                         resourceService.getWritableResource(serializingStorePath), InMemoryStore::new,
                         InMemoryStore.class));
         pieBuilder.withTaskDefs(spoofax.injector.getInstance(GuiceTaskDefs.class));
