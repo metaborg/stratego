05a355d517a3f9b04ea4b15d0930e1a275d0dc01
Toine Hartman <tjbhartman@gmail.com>
2021-09-30 15:32:58 +0200

    Update benchmark problem sizes

--- stratego.build.spoofax2.benchmark/src/main/java/benchmark/stratego2/compilation/stratego/Sieve.java
+++ stratego.build.spoofax2.benchmark/src/main/java/benchmark/stratego2/compilation/stratego/Sieve.java
@@ -6,7 +6,7 @@ import org.openjdk.jmh.annotations.Param;
 
 public class Sieve extends StrategoCompilationBenchmark implements SieveProblem {
 
-    @Param({"20", "100", "1000", "2000", "100000"})
+    @Param({"20", "100", "1000", "2000", "10000"})
     int problemSize;
 
 }
