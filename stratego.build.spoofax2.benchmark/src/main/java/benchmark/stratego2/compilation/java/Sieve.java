7c90d0a0f2b2f57c9f69da7f870ef02486ee0a11
Toine Hartman <tjbhartman@gmail.com>
2021-10-12 12:00:38 +0200

    More reasonable problem sizes for Sieve benchmarks

--- stratego.build.spoofax2.benchmark/src/main/java/benchmark/stratego2/compilation/java/Sieve.java
+++ stratego.build.spoofax2.benchmark/src/main/java/benchmark/stratego2/compilation/java/Sieve.java
@@ -6,7 +6,7 @@ import org.openjdk.jmh.annotations.Param;
 
 public class Sieve extends JavaCompilationBenchmark implements SieveProblem {
 
-    @Param({"20", "100", "1000", "2000", "10000"})
+    @Param({"20", "40", "60", "80", "100", /*"1000", "2000", "100000"*/})
     int problemSize;
 
 }
