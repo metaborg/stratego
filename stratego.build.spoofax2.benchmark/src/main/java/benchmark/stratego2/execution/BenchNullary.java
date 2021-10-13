0048a444ecee4ca42f3b6b86cac555952e3558b7
Toine Hartman <tjbhartman@gmail.com>
2021-10-13 17:35:10 +0200

    Change generated nullary benchmarks

--- stratego.build.spoofax2.benchmark/src/main/java/benchmark/stratego2/execution/BenchNullary.java
+++ stratego.build.spoofax2.benchmark/src/main/java/benchmark/stratego2/execution/BenchNullary.java
@@ -2,15 +2,18 @@ package benchmark.stratego2.execution;
 
 import benchmark.stratego2.problem.BenchNullaryProblem;
 import benchmark.stratego2.template.benchmark.execution.ExecutionBenchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Timeout;
 
 import java.util.concurrent.TimeUnit;
 
 @Timeout(time = 5, timeUnit = TimeUnit.MINUTES)
+@BenchmarkMode({Mode.SampleTime})
 public class BenchNullary extends ExecutionBenchmark implements BenchNullaryProblem {
 
-    @Param({"100", "200", "300", "400", "500", "600", "700", "800", "900", "1000"})
+    @Param({"100", "200", "300", "400", "500", /*"600", "700", "800", "900", "1000"*/})
     int problemSize;
 
 }
