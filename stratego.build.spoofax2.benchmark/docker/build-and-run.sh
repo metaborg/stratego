349396341ddb024e727e05ee0502178f1307a946
Toine Hartman <tjbhartman@gmail.com>
2021-10-13 18:19:12 +0200

    Improve docker container name

--- stratego.build.spoofax2.benchmark/docker/build-and-run.sh
+++ stratego.build.spoofax2.benchmark/docker/build-and-run.sh
@@ -3,8 +3,11 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-CONTAINER=stratego2evaluation
-
 git pull
-docker build -t ${CONTAINER} .
+
+CONTAINER_NAME=stratego2evaluation
+CONTAINER_VERSION=$(git rev-parse --short HEAD)
+CONTAINER=${CONTAINER_NAME}:${CONTAINER_VERSION}
+
+docker build -t ${CONTAINER}
 docker run --rm -d -v ~/stratego2evaluation:/app/data ${CONTAINER}
