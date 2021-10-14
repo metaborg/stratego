ef91f0bea3abbe256f6793494b95db23698024af
Toine Hartman <tjbhartman@gmail.com>
2021-10-14 13:37:49 +0200

    Propagate TARGET env to Docker

--- stratego.build.spoofax2.benchmark/docker/build-and-run.sh
+++ stratego.build.spoofax2.benchmark/docker/build-and-run.sh
@@ -3,11 +3,11 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-git pull
+#git pull
 
 CONTAINER_NAME=stratego2evaluation
 CONTAINER_VERSION=$(git rev-parse --short HEAD)
 CONTAINER=${CONTAINER_NAME}:${CONTAINER_VERSION}
 
 docker build -t ${CONTAINER} .
-docker run --rm -d -v ~/stratego2evaluation:/app/data ${CONTAINER}
+docker run --rm -d -v ~/stratego2evaluation:/app/data -e TARGET=${TARGET} ${CONTAINER}
