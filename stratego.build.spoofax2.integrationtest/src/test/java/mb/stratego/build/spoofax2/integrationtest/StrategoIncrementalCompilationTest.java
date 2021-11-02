bb7655fcc1891afac810a3a47b6d5fe22d8700b2
Toine Hartman <tjbhartman@gmail.com>
2021-11-02 16:28:37 +0100

    Add missing import

--- stratego.build.spoofax2.integrationtest/src/test/java/mb/stratego/build/spoofax2/integrationtest/StrategoIncrementalCompilationTest.java
+++ stratego.build.spoofax2.integrationtest/src/test/java/mb/stratego/build/spoofax2/integrationtest/StrategoIncrementalCompilationTest.java
@@ -7,6 +7,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Objects;
 
 import javax.annotation.Nullable;
