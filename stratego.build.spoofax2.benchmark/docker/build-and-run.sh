f34f04a1a7171d7dcef7557ea630f56e9ee67021
Toine Hartman <tjbhartman@gmail.com>
2021-11-17 17:54:24 +0100

    Notify to copy

--- stratego.build.spoofax2.benchmark/docker/build-and-run.sh
+++ stratego.build.spoofax2.benchmark/docker/build-and-run.sh
@@ -10,6 +10,6 @@ IMAGE=${IMAGE_NAME}:${IMAGE_VERSION}
 CONTAINER=${IMAGE_NAME}_${IMAGE_VERSION}_${RANDOM}
 
 docker build -t ${IMAGE} .
-docker run -d --name ${CONTAINER} -e TARGET=${TARGET:-} --env-file .env ${CONTAINER}
-docker container cp ${CONTAINER}:/home/myuser/data/benchmark_results/ ~/stratego2evaluation/.
-docker container rm ${CONTAINER}
+docker run -d --name ${CONTAINER} -e TARGET=${TARGET:-} -e CONTAINER --env-file .env ${IMAGE}
+# docker container cp ${CONTAINER}:/home/myuser/data/benchmark_results/ ~/stratego2evaluation/.
+# docker container rm ${CONTAINER}
