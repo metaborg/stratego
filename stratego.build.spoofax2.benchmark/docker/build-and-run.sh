2f0b181e4107e970c610949731e9ad6e6daa395f
Toine Hartman <tjbhartman@gmail.com>
2021-11-22 09:52:07 +0100

    Small changes to benchmarking scripts

--- stratego.build.spoofax2.benchmark/docker/build-and-run.sh
+++ stratego.build.spoofax2.benchmark/docker/build-and-run.sh
@@ -10,6 +10,6 @@ IMAGE=${IMAGE_NAME}:${IMAGE_VERSION}
 CONTAINER=${IMAGE_NAME}_${IMAGE_VERSION}_${RANDOM}
 
 docker build -t ${IMAGE} .
-docker run -d --name ${CONTAINER} -e TARGET=${TARGET:-} -e CONTAINER --env-file .env ${IMAGE}
+docker run -d --name ${CONTAINER} -e TARGET=${TARGET:-} -e CONTAINER=${CONTAINER} --env-file .env ${IMAGE}
 # docker container cp ${CONTAINER}:/home/myuser/data/benchmark_results/ ~/stratego2evaluation/.
 # docker container rm ${CONTAINER}
