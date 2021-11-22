aa0deeeaeb4a791f3489435e45325e7904ee0470
Jeff Smits <mail@jeffsmits.net>
2021-11-22 13:22:39 +0100

    Pass list of found modules from Resolve output to CheckOutput

--- stratego.build/src/main/java/mb/stratego/build/strincr/function/GetUnreportedResultFiles.java
+++ stratego.build/src/main/java/mb/stratego/build/strincr/function/GetUnreportedResultFiles.java
@@ -4,9 +4,7 @@ import java.util.LinkedHashSet;
 
 import mb.pie.api.SerializableFunction;
 import mb.resource.hierarchical.ResourcePath;
-import mb.stratego.build.strincr.function.output.Str2LibInfo;
 import mb.stratego.build.strincr.task.output.BackOutput;
-import mb.stratego.build.strincr.task.output.GlobalData;
 
 public class GetUnreportedResultFiles implements SerializableFunction<BackOutput, LinkedHashSet<ResourcePath>> {
     public static final GetUnreportedResultFiles INSTANCE = new GetUnreportedResultFiles();
--- stratego.build/src/main/java/mb/stratego/build/strincr/task/output/CheckOutput.java
+++ stratego.build/src/main/java/mb/stratego/build/strincr/task/output/CheckOutput.java
@@ -13,20 +13,24 @@ import mb.stratego.build.strincr.message.MessageSeverity;
 public class CheckOutput implements Serializable {
     public final LinkedHashMap<StrategySignature, LinkedHashSet<IModuleImportService.ModuleIdentifier>>
         dynamicRuleIndex;
+    public final LinkedHashSet<IModuleImportService.ModuleIdentifier> allModuleIdentifiers;
     public final ArrayList<Message> messages;
     public final boolean containsErrors; // derived from messages
 
     public CheckOutput(
         LinkedHashMap<StrategySignature, LinkedHashSet<IModuleImportService.ModuleIdentifier>> dynamicRuleIndex,
+        LinkedHashSet<IModuleImportService.ModuleIdentifier> allModuleIdentifiers,
         ArrayList<Message> messages, boolean containsErrors) {
         this.dynamicRuleIndex = dynamicRuleIndex;
+        this.allModuleIdentifiers = allModuleIdentifiers;
         this.messages = messages;
         this.containsErrors = containsErrors;
     }
 
     public CheckOutput(LinkedHashMap<StrategySignature, LinkedHashSet<IModuleImportService.ModuleIdentifier>> dynamicRuleIndex,
+        LinkedHashSet<IModuleImportService.ModuleIdentifier> allModuleIdentifiers,
         ArrayList<Message> messages) {
-        this(dynamicRuleIndex, messages,
+        this(dynamicRuleIndex, allModuleIdentifiers, messages,
             messages.stream().anyMatch(m -> m.severity == MessageSeverity.ERROR));
     }
 
@@ -40,11 +44,14 @@ public class CheckOutput implements Serializable {
 
         if(!dynamicRuleIndex.equals(output.dynamicRuleIndex))
             return false;
+        if(!allModuleIdentifiers.equals(output.allModuleIdentifiers))
+            return false;
         return messages.equals(output.messages);
     }
 
     @Override public int hashCode() {
         int result = dynamicRuleIndex.hashCode();
+        result = 31 * result + allModuleIdentifiers.hashCode();
         result = 31 * result + messages.hashCode();
         return result;
     }
