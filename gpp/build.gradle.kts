5f265ec35cab87b0cb9491dc082b3498f536385f
Gabriel Konat <gabrielkonat@gmail.com>
2022-01-10 14:31:54 +0100

    Add Gradle build file for gpp Stratego 2 library.

---
+++ gpp/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+  id("org.metaborg.gradle.config.java-library")
+  id("org.metaborg.devenv.spoofax.gradle.langspec")
+  id("de.set.ecj") // Use ECJ to speed up compilation of Stratego's generated Java files.
+  `maven-publish`
+}
+
+// Replace language dependencies with overridden/local ones.
+fun compositeBuild(name: String) = "$group:$name:$version"
+val spoofax2BaselineVersion: String by ext
+val spoofax2Version: String by ext
+spoofaxLanguageSpecification {
+  addCompileDependenciesFromMetaborgYaml.set(false)
+  addSourceDependenciesFromMetaborgYaml.set(false)
+}
+dependencies {
+  compileLanguage(compositeBuild("org.metaborg.meta.lang.esv"))
+  compileLanguage(compositeBuild("org.metaborg.meta.lang.template"))
+  compileLanguage(project(":stratego.lang"))
+
+  sourceLanguage(project(":strategolib"))
+  sourceLanguage(project(":stratego.lang"))
+  sourceLanguage(project(":org.metaborg.meta.lang.stratego"))
+
+  compileOnly(project(":strategolib"))
+}
+
+ecj {
+  toolVersion = "3.21.0"
+}
+tasks.withType<JavaCompile> { // ECJ does not support headerOutputDirectory (-h argument).
+  options.headerOutputDirectory.convention(provider { null })
+}
