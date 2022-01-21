72edb375dcf16a80536c55c6b9db77196320a957
Gabriel Konat <gabrielkonat@gmail.com>
2022-01-21 11:11:40 +0100

    Prevent duplicate publication.

--- gpp/build.gradle.kts
+++ gpp/build.gradle.kts
@@ -25,6 +25,13 @@ dependencies {
   compileOnly(project(":strategolib"))
 }
 
+metaborg {
+  // Do not create Java publication; this project is already published as a Spoofax 2 language.
+  javaCreatePublication = false
+  javaCreateSourcesJar = false
+  javaCreateJavadocJar = false
+}
+
 ecj {
   toolVersion = "3.21.0"
 }
--- strategolib/build.gradle.kts
+++ strategolib/build.gradle.kts
@@ -17,6 +17,12 @@ dependencies {
   compileLanguage(project(":stratego.lang"))
 }
 
+metaborg { // Do not create Java publication; this project is already published as a Spoofax 2 language.
+  javaCreatePublication = false
+  javaCreateSourcesJar = false
+  javaCreateJavadocJar = false
+}
+
 ecj {
   toolVersion = "3.21.0"
 }
