d49f0db28be3a0d8e3bc2b4157d0c6a6303f4fa3
rensux <renzo.russel@gmail.com>
2023-12-29 01:59:42 +0100

    added TIL benchmarks

---
+++ stratego.build.spoofax2.benchmark/src/main/java/renzo/til/problems/ExecutableProblem.java
@@ -0,0 +1,25 @@
+package renzo.til.problems;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+public enum ExecutableProblem {
+    Add100("add100", "0"),
+    Add200("add200", "0"),
+    Add500("add500", "0"),
+    Add1000("add1000", "0"),
+    EBlock("eblock", "1,1"),
+    Factorial4("factorial", "4"),
+    Factorial5("factorial", "5"),
+    Factorial6("factorial", "6"),
+    Factorial7("factorial", "7"),
+    Factorial8("factorial", "8"),
+    Factorial9("factorial", "9");
+
+    public final String name;
+    public final Collection<String> input;
+    ExecutableProblem(String name, String input) {
+        this.name = name;
+        this.input = Arrays.asList(input.split(","));
+    }
+}
